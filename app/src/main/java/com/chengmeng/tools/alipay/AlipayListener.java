063fd10bf53c0e46b0d5adb0a1fe1ab678c148d8
linorz <965035290@qq.com>
2017-06-03 12:59:08 +0800

    添加支付宝SDK，初步测试支付功能，只能单方向支付，未加入交易流程，待完善

---
+++ app/src/main/java/com/chengmeng/tools/alipay/AlipayListener.java
@@ -0,0 +1,12 @@
+package com.chengmeng.tools.alipay;
+
+public interface AlipayListener {
+
+    /**
+     * 支付是否成功
+     *
+     * @param is_success 是否支付成功
+     */
+    public void onResult(boolean is_success);
+
+}
